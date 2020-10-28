fb5b3ae09339e929282738c9eb80e76c25304e00
Saurav Tiwary <srv.twry@gmail.com>
2020-10-28 20:14:55 +0530

    Add internal key comparator skeleton
    Signed-off-by: Saurav Tiwary <srv.twry@gmail.com>

--- pkg/storage/internalkey.go
+++ pkg/storage/internalkey.go
@@ -1,5 +1,7 @@
 package storage
 
+import "bytes"
+
 // internalKey is the key used for memtable and SST in the db.
 //
 // It consists of the user key along with a 8-byte suffix.
@@ -34,4 +36,33 @@ func (ik internalKey) kind() internalKeyKind {
 // sequenceNumber returns the sequence number of the internal key.
 func (ik internalKey) sequenceNumber() uint64 {
 	panic("Not implemented")
+}
+
+// valid returns if the internal key is valid structurally.
+func (ik internalKey) valid() bool {
+	panic("Not implemented")
+}
+
+// internalKeyComparator is the comparator which uses a user key comparator to compare internal key.
+//
+// keys are first compared for their user key according to the user key comparator.
+// ties are broken by comparing sequence number (decreasing) and then by kind (decreasing).
+type internalKeyComparator struct {
+	userKeyComparator Comparator
+}
+
+func (d internalKeyComparator) Compare(a, b []byte) int {
+	return bytes.Compare(a, b)
+}
+
+func (d internalKeyComparator) Name() string {
+	return "InternalKeyComparator"
+}
+
+// newInternalKeyComparator creates a new instance of an internalKeyComparator
+// returns a pointer to the internalKeyComparator struct.
+func newInternalKeyComparator(userKeyComparator Comparator) *internalKeyComparator {
+	return &internalKeyComparator{
+		userKeyComparator: userKeyComparator,
+	}
 }
