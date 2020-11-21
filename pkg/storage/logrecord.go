561ff2d724985ad11f7c8e3f61579882d4fd7c38
Saurav Tiwary <srv.twry@gmail.com>
2020-11-21 21:18:27 +0530

    add next func in log record writer
    Signed-off-by: Saurav Tiwary <srv.twry@gmail.com>

--- pkg/storage/logrecord.go
+++ pkg/storage/logrecord.go
@@ -1,6 +1,10 @@
 package storage
 
-import "io"
+import (
+	"io"
+
+	log "github.com/sirupsen/logrus"
+)
 
 // The log record format details can be found at the below link.
 // https://github.com/google/leveldb/blob/master/doc/log_format.md
@@ -36,8 +40,18 @@ type logRecordWriter struct {
 
 	lastRecordOffset int64
 
+	// pending indicates if there is a chunk that is yet to written but is buffered.
+	pending bool
+
+	// first indicates if the current chunk is the first chunk of the record.
+	first bool
+
 	// err is any error encountered during any log record writer operation.
 	err error
+}
+
+func (lrw *logRecordWriter) writeBlock() {
+	panic("Not Implemented")
 }
 
 // newLogRecordWriter creates a new log record writer.
@@ -57,10 +71,58 @@ func newLogRecordWriter(w io.Writer) *logRecordWriter {
 	}
 }
 
+// next returns a io.Writer for the next record.
 func (lrw *logRecordWriter) next() (io.Writer, error) {
-	return lrw.w, nil
+	lrw.seq++
+	if lrw.err != nil {
+		log.WithFields(log.Fields{"error": lrw.err.Error()}).Error("storage::logrecord: next; existing background error found in the log record writer.")
+		return nil, lrw.err
+	}
+
+	log.WithFields(log.Fields{"logRecordWriter": lrw}).Info("storage::logrecord: next; next called on the log record writer.")
+
+	if lrw.pending {
+		log.Info("storage::logrecord: next; found pending chunk.")
+		// todo: fill the header of this pending chunk.
+	}
+
+	// move pointers for the next chunk headers
+	lrw.lo = lrw.hi
+	lrw.hi = lrw.hi + headerSize
+
+	// check if there is enough size to fit in at least the header and one byte of data.
+	// check the link at the start for more.
+	if lrw.hi > blockSize {
+		log.Info("storage::logrecord: next; not enough space in current block.")
+
+		// fill the rest with zeroes
+		for x := lrw.lo; x < blockSize; x++ {
+			lrw.buf[x] = 0
+		}
+
+		lrw.writeBlock()
+
+		if lrw.err != nil {
+			log.WithFields(log.Fields{"error": lrw.err.Error()}).Error("storage::logrecord: next; error in writing the block.")
+			return nil, lrw.err
+		}
+	}
+
+	lrw.lastRecordOffset = lrw.baseOffset + lrw.blockNumber*blockSize + int64(lrw.lo)
+	lrw.first = true
+	lrw.pending = true
+	return singleLogRecordWriter{lrw, lrw.seq}, nil
 }
 
 func (lrw *logRecordWriter) close() error {
 	return nil
+}
+
+type singleLogRecordWriter struct {
+	w   *logRecordWriter
+	seq int
+}
+
+func (slrw singleLogRecordWriter) Write(p []byte) (int, error) {
+	panic("Not implemented")
 }
