54758372d07bc3c0b36024b3425274a56e413117
Saurav Tiwary <sauravtiwary@google.com>
2021-06-07 21:10:31 +0530

    feat: implement rpc methods for client

--- pkg/icecanesql/rpc.go
+++ pkg/icecanesql/rpc.go
@@ -60,8 +60,94 @@ func (r *rpcRepository) createAndStoreConn() error {
 	return nil
 }
 
+// beginTxn makes a BeginTxn RPC to init a txn
+func (r *rpcRepository) beginTxn(readOnly bool) (nxtId uint64, err error) {
+	log.Info("icecanesql::rpc::beginTxn; start;")
+
+	nxtId = 0
+
+	err = r.createAndStoreConn()
+	if err != nil {
+		return nxtId, err
+	}
+
+	client := pb.NewIcecaneKVClient(r.kvConn)
+	mode := pb.TxnMode_ReadWrite
+	if readOnly {
+		mode = pb.TxnMode_ReadOnly
+	}
+	req := &pb.BeginTxnRequest{
+		Mode: mode,
+	}
+
+	resp, err := client.BeginTxn(context.Background(), req)
+	if err != nil {
+		log.Error(fmt.Sprintf("icecanesql::rpc::beginTxn; error in grpc request: %v", err))
+		return nxtId, err
+	} else if resp.Error != nil {
+		log.Error(fmt.Sprintf("icecanesql::rpc::beginTxn; error response from the kv server: %v", resp.Error))
+		return nxtId, fmt.Errorf(resp.Error.Message)
+	}
+
+	nxtId = resp.TxnId
+	return nxtId, nil
+}
+
+// commitTxn makes a CommitTxn RPC to init a txn
+func (r *rpcRepository) commitTxn(txnID uint64) (err error) {
+	log.Info("icecanesql::rpc::commitTxn; start;")
+
+	err = r.createAndStoreConn()
+	if err != nil {
+		return err
+	}
+
+	client := pb.NewIcecaneKVClient(r.kvConn)
+	req := &pb.CommitTxnRequest{
+		TxnId: txnID,
+	}
+
+	resp, err := client.CommitTxn(context.Background(), req)
+	if err != nil {
+		log.Error(fmt.Sprintf("icecanesql::rpc::commitTxn; error in grpc request: %v", err))
+		return err
+	} else if resp.Error != nil {
+		log.Error(fmt.Sprintf("icecanesql::rpc::commitTxn; error response from the kv server: %v", resp.Error))
+		return fmt.Errorf(resp.Error.Message)
+	}
+
+	return nil
+}
+
+// rollbackTxn makes a RollbackTxn RPC to init a txn
+func (r *rpcRepository) rollbackTxn(txnID uint64) (err error) {
+	log.Info("icecanesql::rpc::rollbackTxn; start;")
+
+	err = r.createAndStoreConn()
+	if err != nil {
+		return err
+	}
+
+	client := pb.NewIcecaneKVClient(r.kvConn)
+	req := &pb.RollbackTxnRequest{
+		TxnId: txnID,
+	}
+
+	resp, err := client.RollbackTxn(context.Background(), req)
+	if err != nil {
+		log.Error(fmt.Sprintf("icecanesql::rpc::rollbackTxn; error in grpc request: %v", err))
+		return err
+	} else if resp.Error != nil {
+		log.Error(fmt.Sprintf("icecanesql::rpc::rollbackTxn; error response from the kv server: %v", resp.Error))
+		return fmt.Errorf(resp.Error.Message)
+	}
+
+	return nil
+}
+
 // get makes a Get RPC call to the kv store
 func (r *rpcRepository) get(key []byte, txnID uint64) (k, v []byte, err error) {
+	log.Info("icecanesql::rpc::get; start;")
 	err = r.createAndStoreConn()
 	if err != nil {
 		return nil, nil, err
@@ -90,6 +176,7 @@ func (r *rpcRepository) get(key []byte, txnID uint64) (k, v []byte, err error) {
 
 // set makes a Set RPC call to the kv store
 func (r *rpcRepository) set(key, value []byte, txnID uint64) (bool, error) {
+	log.Info("icecanesql::rpc::set; start;")
 	err := r.createAndStoreConn()
 	if err != nil {
 		return false, err
@@ -108,6 +195,32 @@ func (r *rpcRepository) set(key, value []byte, txnID uint64) (bool, error) {
 		return false, err
 	} else if resp.Error != nil {
 		log.Error(fmt.Sprintf("icecanesql::rpc::set; error response from the kv server: %v", resp.Error))
+		return false, fmt.Errorf(resp.Error.Message)
+	}
+
+	return resp.Success, nil
+}
+
+// delete makes a Delete RPC call to the kv store
+func (r *rpcRepository) delete(key []byte, txnID uint64) (bool, error) {
+	log.Info("icecanesql::rpc::delete; start;")
+	err := r.createAndStoreConn()
+	if err != nil {
+		return false, err
+	}
+
+	client := pb.NewIcecaneKVClient(r.kvConn)
+	req := &pb.DeleteRequest{
+		Key:   key,
+		TxnId: txnID,
+	}
+
+	resp, err := client.Delete(context.Background(), req)
+	if err != nil {
+		log.Error(fmt.Sprintf("icecanesql::rpc::delete; error in grpc request: %v", err))
+		return false, err
+	} else if resp.Error != nil {
+		log.Error(fmt.Sprintf("icecanesql::rpc::delete; error response from the kv server: %v", resp.Error))
 		return false, fmt.Errorf(resp.Error.Message)
 	}
 
